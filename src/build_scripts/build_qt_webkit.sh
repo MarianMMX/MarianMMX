d7692a076f214c2f688f29602a72deeace50b740
MarianMMX <marianmmx@gmail.com>
2016-01-14 22:38:34 +0200

    qtwebkit

--- src/build_scripts/build_qt_webkit.sh
+++ src/build_scripts/build_qt_webkit.sh
@@ -11,8 +11,16 @@ if [ ! -d "qtwebkit" ]; then
 fi
 cd qtwebkit
 
-$BUILD_SCRIPTS_DIR/config_qt.sh
-make
+SQLITE3SRCDIR="$QT_SRC_BASE_DIR"/qtbase/src/3rdparty/sqlite
+export SQLITE3SRCDIR
+
+$BUILD_DIR/qtbase/bin/qmake $QT_SRC_BASE_DIR/qtwebkit/WebKit.pro
+if [ "$1" = "go" ]; then
+  echo "Make"
+  make
+else
+	echo "Call $0 go for make"
+fi
 
 cd $O_DIR
 
