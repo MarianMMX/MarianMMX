141e1db1a4b3e34502f6b0c6dd8cdd1b7eef47d0
MarianMMX <marianmmx@gmail.com>
2016-01-14 16:17:55 +0200

    Update build_icu.sh

--- src/build_scripts/build_icu.sh
+++ src/build_scripts/build_icu.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+# 2016-01-14 16:30 MarianMMX
+
 . $(dirname $0)/env_qt.sh
 T_DIR="$(pwd)"/icu
 
+sh runConfigureICU FreeBSD --prefix=$T_DIR --enable-static --disable-shared
+make
+make install
