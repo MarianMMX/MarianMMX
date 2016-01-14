fe9b4d0b39418c633ff83db285ae1fa8dd225445
MarianMMX <marianmmx@gmail.com>
2016-01-14 16:43:38 +0200

    env

--- src/build_scripts/env_qt.sh
+++ src/build_scripts/env_qt.sh
@@ -1,5 +1,15 @@
-CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:"$(pwd)"/../QtExtra/icu/include/:"$(pwd)"/../QtExtra/openssl/include/
-export CPLUS_INCLUDE_PATH
-LIBRARY_PATH=$LIBRARY_PATH:"$(pwd)"/../QtExtra/icu/lib/
-export LIBRARY_PATH
+#!/bin/sh
+# 2016-01-14 14:54 MarianMMX
 
+QT_SRC_BASE_DIR=$(dirname $0)/../3rdparty/qt5
+QT_EXTRA_DIR="$(pwd)"/../QtExtra
+
+TRD_ICU_SRC_DIR=$(dirname $0)/../3rdparty/icu
+export $TRD_ICU_SRC_DIR
+TRD_OPENSSL_SRC_DIR=$(dirname $0)/../3rdparty/openssl
+export $TRD_OPENSSL_SRC_DIR
+
+CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$QT_EXTRA_DIR/icu/include/:$QT_EXTRA_DIR/openssl/include/
+export CPLUS_INCLUDE_PATH
+LIBRARY_PATH=$LIBRARY_PATH:$QT_EXTRA_DIR/icu/lib/
+export LIBRARY_PATH
