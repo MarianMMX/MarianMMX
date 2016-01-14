9cb9bfbe8c99b58562d30ae410c583134bb347ca
MarianMMX openshift local <root@broker.openshift.local>
2016-01-14 10:19:43 -0500

    temp

--- src/build_scripts/build_icu.sh
+++ src/build_scripts/build_icu.sh
@@ -4,6 +4,16 @@
 . $(dirname $0)/env_qt.sh
 T_DIR="$(pwd)"/icu
 
-sh runConfigureICU --prefix=$T_DIR --enable-static --disable-shared
+#echo $TRD_ICU_SRC_DIR
+#exit
+
+if [ -z "$1" ]; then
+  echo "Eroare platforma $0 Linux/gcc"
+  exit 1
+fi
+
+chmod +x "$TRD_ICU_SRC_DIR"/source/runConfigureICU
+chmod +x "$TRD_ICU_SRC_DIR"/source/configure
+sh "$TRD_ICU_SRC_DIR"/source/runConfigureICU "$1" --prefix=$T_DIR --enable-static --disable-shared
 make
 make install
