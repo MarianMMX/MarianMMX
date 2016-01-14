305f1450adba7beacd3a612df01715e48a102002
MarianMMX <marianmmx@gmail.com>
2016-01-14 17:00:44 +0200

    env

--- src/build_scripts/env_qt.sh
+++ src/build_scripts/env_qt.sh
@@ -5,9 +5,9 @@ QT_SRC_BASE_DIR=$(dirname $0)/../3rdparty/qt5
 QT_EXTRA_DIR="$(pwd)"/../QtExtra
 
 TRD_ICU_SRC_DIR=$(dirname $0)/../3rdparty/icu
-export $TRD_ICU_SRC_DIR
+export TRD_ICU_SRC_DIR
 TRD_OPENSSL_SRC_DIR=$(dirname $0)/../3rdparty/openssl
-export $TRD_OPENSSL_SRC_DIR
+export TRD_OPENSSL_SRC_DIR
 
 CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$QT_EXTRA_DIR/icu/include/:$QT_EXTRA_DIR/openssl/include/
 export CPLUS_INCLUDE_PATH
